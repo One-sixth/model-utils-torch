b7cedb14b044038ab777eb0abdf713dfe4a38da5
OneSixth <One-sixth@users.noreply.github.com>
2022-06-21 11:56:27 +0800

    add install cmd

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup  # , find_packages
+
+ver_str = '0.0.1'
+
+setup(
+    name='model-utils-torch',
+    version=ver_str,
+    description='Personal pytorch devkit.',
+    author='onesixth',
+    author_email='noexist@noexist.noexist',
+    url='https://github.com/One-sixth/model-utils-torch',
+    install_requires=[],
+    entry_points={'console_scripts': []},
+    packages=['model_utils_torch'],
+    package_data={},
+)
